e2c7c6b770b9cec9dfd1d0d4d2f73c6b79445ea1
itsokdev <pawardevesh03@gmail.com>
2026-01-18 21:57:35 +0530

    Added menu-driven interface

--- src/main.py
+++ src/main.py
@@ -15,12 +15,22 @@ def add_job(company, role, location, status):
         ])
     print("✅ Job added successfully")
 
+#Day 2: View Jobs Functionality
+def view_jobs():
+    with open("data/jobs.csv", "r") as file:
+        reader = csv.reader(file)
+        print("Your Job Applications:")
+        for row in reader:
+            print(f"Company: {row[0]}, Role: {row[1]}, Location: {row[2]}, Status: {row[3]}, Date Applied: {row[4]}")
+
+
 #Day 1: Main Loop
 if __name__ == "__main__":
     while True:
         print("Job Application Tracker")
         print("1. Add Job")
-        print("2. Exit")
+        print("2. View Jobs")
+        print("3. Exit")
         choice = input("Choose an option: ")
 
         if choice == "1":
@@ -30,6 +40,11 @@ if __name__ == "__main__":
             status = input("Status (e.g., Applied, Interviewing, Offered): ")
             add_job(company, role, location, status)
         elif choice == "2":
+            view_jobs()
+        elif choice == "3":
             break
         else:
             print("Invalid choice. Please try again.")
+
+
+        
